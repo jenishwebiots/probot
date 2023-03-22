0a27d8b5738901117b899643649b196ca239d02a
charmiswebiots <53256870+charmiswebiots@users.noreply.github.com>
2023-03-22 16:27:25 +0530

    regenerate chat functionality and API modification

--- lib/screens/app_screens/chat_layout/layouts/sender_width_text.dart
+++ lib/screens/app_screens/chat_layout/layouts/sender_width_text.dart
@@ -1,28 +1,29 @@
-
-
 import '../../../../config.dart';
 
 class SenderWidthText extends StatelessWidget {
   final String? text;
-  const SenderWidthText({Key? key,this.text}) : super(key: key);
+  const SenderWidthText({Key? key, this.text}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: 250,
-      padding: const EdgeInsets.symmetric(
-          horizontal: Insets.i10, vertical: Insets.i12),
-      decoration: BoxDecoration(
-          color: appCtrl.appTheme.boxBg,
-          borderRadius:
-          BorderRadius.circular(AppRadius.r6)),
-      child: Text(
-        text!,
-        overflow: TextOverflow.clip,
-        style: AppCss.outfitMedium14
-            .textColor(appCtrl.appTheme.txt)
-            .textHeight(1.2),
-      ),
-    );
+    return GetBuilder<ChatLayoutController>(builder: (chatCtrl) {
+      return Container(
+        width: 250,
+        padding: const EdgeInsets.symmetric(
+            horizontal: Insets.i10, vertical: Insets.i12),
+        decoration: BoxDecoration(
+            color: appCtrl.appTheme.boxBg,
+            borderRadius: BorderRadius.circular(AppRadius.r6)),
+        child: Text(
+          text!,
+          overflow: TextOverflow.clip,
+          style: AppCss.outfitMedium14
+              .textColor(appCtrl.appTheme.txt)
+              .textHeight(1.2),
+        ),
+      ).inkWell(
+          onTap: () =>
+              {chatCtrl.chatController.text = text!, chatCtrl.proccessChat()});
+    });
   }
 }
